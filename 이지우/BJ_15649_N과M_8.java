1bde27f6097f4b7045610c8342b1564f010991aa
jiwoo <ckdn9595@gmail.com>
2022-07-20 20:00:10 +0900

    ✅success: boj N과M 7~9

---
+++ 이지우/BJ_15649_N과M_8.java
@@ -0,0 +1,50 @@
+package A202207;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_15649_N과M_8 {
+	static StringBuilder sb = new StringBuilder();
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N,M;
+	static int[] values, numbers;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.valueOf(st.nextToken());
+		M = Integer.valueOf(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		numbers = new int[M];
+		values = new int[N+1];
+		visited = new boolean[N+1];
+		for(int i = 1 ; i <= N ; i++) {
+			values[i] = Integer.valueOf(st.nextToken());
+		}
+		Arrays.sort(values);
+		dp(0);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+	private static void dp(int r) {
+		if(r >= M) {
+			for(int a : numbers)sb.append(a + " ");
+			sb.append("\n");
+			return;
+		}
+		for(int i = 1; i <= N; i++) {
+			if(visited[i] || numbers[r] == values[i])continue;
+			visited[i] = true;
+			numbers[r] = values[i];
+			dp(r+1);
+			visited[i] = false;
+		}
+		numbers[r] = 0;
+	}
+}
