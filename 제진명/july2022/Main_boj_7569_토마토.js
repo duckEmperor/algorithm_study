62424e05b4ab7a1108d8d55086bcfc10f409b5ad
jinmyeong <1995jjm@naver.com>
2022-08-01 20:45:38 +0900

    ✅ success: boj 7569 토마토

--- 제진명/july2022/Main_boj_7569_토마토.js
+++ 제진명/july2022/Main_boj_7569_토마토.js
@@ -1,59 +1,53 @@
 let fs = require('fs');
-let input = fs.readFileSync('example.txt').toString();
+let input = fs.readFileSync('example.txt').toString().split('\n');
 
-let readline = input.split(`\n`);
+const D = [[0,0,1],[0,0,-1],[-1,0,0],[1,0,0],[0,-1,0],[0,1,0]];
 
-let lineSplit = readline[0].split(' ');
-
-let M = parseInt(lineSplit[0]);
-let N = parseInt(lineSplit[1]);
-let H = parseInt(lineSplit[2]);
-
-let tomatoBox = Array.from(Array(H), () => Array.from(Array(N), () => new Array(M).fill(0)));
+const [M,N,H] = input.shift().split(' ').map(Number);
 
 let queue = [];
 let visited = Array.from(Array(H), () => Array.from(Array(N), () => new Array(M).fill(false)));
 
-let amount = 0;
+let count = M*N*H;
+let z = 0;
+let ans;
 
-for (let i = 0 ; i < H ; i++) {
-    for (let j = 0 ; j < N ; j++) {
-        lineSplit = readline[j+i*N+1].split(' ');
-        for (let k = 0 ; k < M ; k++) {
-            tomatoBox[i][j][k] = parseInt(lineSplit[k]);
-            if (tomatoBox[i][j][k] === 1) {
-                queue.push([i, j, k, 0]);
-                visited[i][j][k] = true;
-            }
-
-            if (tomatoBox[i][j][k] !== -1) {
-                amount += 1;
-            }
+for (let i = 0 ; i < input.length ; i++) {
+    let box = input[i].split(' ').map(Number);
+    box.forEach((tomato, pos) => {
+        if (tomato === 1) {
+            queue.push([i % N, pos, z, 0]);
+            visited[z][i % N][pos] = true;
+            count--;
+        } else if (tomato === -1) {
+            visited[z][i % N][pos] = 1;
+            count--;
         }
-    }
+    });
+
+    if((i+1) % N === 0) ++z;
 }
 
-let D = [[0,0,1],[0,0,-1],[-1,0,0],[1,0,0],[0,-1,0],[0,1,0]];
+let idx = 0;
 
-let ans = 0;
-let count = queue.length;
-
-while(queue.length !== 0) {
-    const [x, y, z, day] = queue.pop();
-    if (ans <= day) ans = day;
-
-    for (let d = 0 ; d < 6 ; d++) {
+while (queue.length != idx) {
+    const [x, y, z, pos] = queue[idx];
+    for (let d = 0 ; d < D.length ; d++) {
         const nx = x + D[d][0];
         const ny = y + D[d][1];
         const nz = z + D[d][2];
 
-        if (nx < 0 || ny < 0 || nz < 0 || nx >= H || ny >= N || nz >= M || visited[nx][ny][nz] || tomatoBox[nx][ny][nz] === -1) continue;
+        if (nx < 0 || ny < 0 || nz < 0 || nx >= N || ny >= M || nz >= H) continue;
 
-        queue.unshift([nx, ny, nz, day+1]);
-        count += 1;
-        visited[nx][ny][nz] = true;
+        if (!visited[nz][nx][ny]) {
+            visited[nz][nx][ny] = true;
+            queue.push([nx, ny, nz, pos+1]);
+            count--;
+        }
     }
+
+    idx++;
+    ans = pos;
 }
 
-if (amount !== count) console.log(-1)
-else console.log(ans);
+console.log(count ? -1 : ans);
