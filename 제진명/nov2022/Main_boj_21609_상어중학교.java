877737b9b2095174e502ae10d6f8c394e11f0ae2
jinmyeong <1995jjm@naver.com>
2022-11-08 15:28:57 +0900

    ✅ success: boj 21609 상어중학교

--- 제진명/nov2022/Main_boj_21609_상어중학교.java
+++ 제진명/nov2022/Main_boj_21609_상어중학교.java
@@ -8,10 +8,19 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_boj_21609_상어중학교 {
+
+    static class Pos {
+        int x, y;
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     static int N, M, ans;
     static int [][] map;
-    static int [][] D = {{0, -1},{1, 0},{0, 1},{-1, 0}};
-    static int amount;
+    static int [][] D = {{0, 1},{0, -1},{1, 0},{-1, 0}};
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,158 +28,160 @@ public class Main_boj_21609_상어중학교 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        amount = N*N;
         map = new int [N][N];
 
         for (int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int  j = 0 ; j < N ; j++) {
+            for (int j = 0 ; j < N ; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == -1) amount--;
             }
         }
 
-        ans = 0;
-        while (amount != 0) {
-            if(!findLargestBlock()) break;
+        while(findLargestBlock());
+
+        System.out.println(ans);
+    }
+
+    static boolean findLargestBlock() {
+        boolean [][] visited = new boolean[N][N];
+        Queue<Pos> queue = new LinkedList<>();
+
+        int largestBlockCnt = 0;
+        int includeRainbowBlockCnt = 0;
+        Pos pos = null;
+
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < N ; j++) {
+                // 행 우선, 열 나중으로 반복문 순회하며 검정 블록과 무지개 불록이 아닌 블록 탐색
+                // 검정 블록 => -1 , 무지개 블록 => 0 이므로 0보다 블록이 숫자 블록, 이미 확인한 숫자 블록은 제외
+                if (map[i][j] <= 0 || visited[i][j]) continue;
+
+                int currentBlocksCnt = 1;
+                int currentRainbowBlocksCnt = 0;
+                visited[i][j] = true;
+                queue.offer(new Pos(i, j));
+
+                while (!queue.isEmpty()) {
+                    Pos now = queue.poll();
+
+                    for (int d = 0 ; d < 4 ; d++) {
+                        int nx = now.x + D[d][0];
+                        int ny = now.y + D[d][1];
+
+                        if (checkOverflow(nx, ny) || visited[nx][ny] || (map[nx][ny] != 0 && map[nx][ny] != map[i][j])) continue;
+
+                        visited[nx][ny] = true;
+                        currentBlocksCnt++;
+                        if (map[nx][ny] == 0) currentRainbowBlocksCnt++;
+
+                        queue.offer(new Pos(nx, ny));
+                    }
+                }
+
+                // 다 돌고 나면 무지개 블록의 방문을 해제
+                for (int k = 0 ; k < N ; k++) {
+                    for (int l = 0 ; l < N ; l++) {
+                        if (map[k][l] == 0) visited[k][l] = false;
+                    }
+                }
+
+                if (currentBlocksCnt >= 2) {
+                    // 현재 연속된 블록의 개수가 기존 블록의 개수보다 많은 경우
+                    // 블록의 최대 개수, 무지개 블록 개수, 좌표를 현재 값으로 초기화
+                    if (currentBlocksCnt > largestBlockCnt) {
+                        pos = new Pos(i, j);
+                        largestBlockCnt = currentBlocksCnt;
+                        includeRainbowBlockCnt = currentRainbowBlocksCnt;
+                    }
+                    // 현재 연속된 블록의 개수가 기존 블로그이 개수와 동일한 경우
+                    else if (currentBlocksCnt == largestBlockCnt) {
+                        // 현재 블록들에 포함된 무지개 블록 개수가 기존 무지개 블록 개수보다 많을 경우
+                        // 무지개 블록 개수를 초기화 하고 좌표를 현재 값으로 초기화
+                        if (currentRainbowBlocksCnt > includeRainbowBlockCnt) {
+                            includeRainbowBlockCnt = currentRainbowBlocksCnt;
+                            pos = new Pos(i, j);
+                        }
+                        // 현재 블록들에 포함된 무지개 블록 개수가 기존 무지개 블록 개수와 동일한 경우
+                        // 좌표만 초기화
+                        else if (currentRainbowBlocksCnt == includeRainbowBlockCnt) {
+                            pos = new Pos(i, j);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (pos != null) {
+            ans += largestBlockCnt * largestBlockCnt;
+            removeBlocks(pos);
             gravity();
             map = rotateMap();
             gravity();
+            return true;
         }
+        else return false;
 
-//        printMap();
-
-        System.out.println(ans);
-
-    }
-
-    static int [][] rotateMap() {
-        int [][] newMap =  new int [N][N];
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < N ; j++) {
-               newMap[N-i-1][j] = map[j][i];
-            }
-        }
-        return newMap;
     }
 
     static void gravity() {
         for (int i = 0 ; i < N ; i++) {
             for (int j = N-2 ; j >= 0 ; j--) {
-                if (map[j][i] > -1) {
-                    int y = j;
-                    for (int k = j+1 ; k < N ; k++) {
-                        if (map[k][i] == -2) {
-                            y = k;
-                        } else break;
-                    }
-                    if (j != y) {
-                        map[y][i] = map[j][i];
-                        map[j][i] = -2;
-                    }
+                if (map[j][i] < 0) continue;
+
+                int nx = j;
+
+                for (int k = j + 1 ; k < N ; k++) {
+                    if (map[k][i] != -2) break;
+                    nx = k;
                 }
+
+                if (nx == j) continue;
+
+                map[nx][i] = map[j][i];
+                map[j][i] = -2;
             }
         }
-
     }
 
-    static boolean findLargestBlock() {
+    static void removeBlocks(Pos pos) {
+        Queue<Pos> queue = new LinkedList<>();
         boolean [][] visited = new boolean[N][N];
-        Queue<int []> queue = new LinkedList<>();
-        int cnt = 0;
-        int x = -1;
-        int y = -1;
-        int rainbowBlockCnt = 0;
-
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < N ; j++) {
-                if (map[i][j] > 0 && !visited[i][j]) {
-                    int temp = 1;
-                    int rainbowTemp = 0;
-                    visited[i][j] = true;
-                    queue.offer(new int [] {i, j});
-                    while (!queue.isEmpty()) {
-                        int [] now = queue.poll();
-
-                        for (int d = 0 ; d < 4 ; d++) {
-                            int nx = now[0] + D[d][0];
-                            int ny = now[1] + D[d][1];
-
-                            if (isOverflow(nx, ny) || (map[nx][ny] != 0 && map[nx][ny] != map[i][j]) || visited[nx][ny]) continue;
-
-                            visited[nx][ny] = true;
-                            temp++;
-                            if(map[nx][ny] == 0) rainbowTemp++;
-                            queue.offer(new int [] {nx, ny});
-                        }
-                    }
-
-//                    System.out.println(map[i][j] + " cnt:" + temp);
-
-                    if (temp > cnt) {
-                        cnt = temp;
-                        x = i; y = j;
-                        rainbowBlockCnt = rainbowTemp;
-                    } else if (temp == cnt && rainbowBlockCnt < rainbowTemp) {
-                        rainbowBlockCnt = rainbowTemp;
-                        x = i; y = j;
-                    } else if (temp == cnt && rainbowBlockCnt == rainbowTemp) {
-                        x = i; y = j;
-                    }
-                }
-            }
-        }
-
-        if (cnt < 2) return false;
-
-        removeBlock(x, y);
-        calculate(cnt);
-        amount -= cnt;
-
-        return true;
-
-    }
-
-
-    static void calculate(int cnt) {
-        ans += cnt * cnt;
-    }
-
-    static void removeBlock(int x, int y) {
-        Queue<int []> queue = new LinkedList<>();
-        boolean [][] visited = new boolean[N][N];
-        visited[x][y] = true;
-        queue.offer(new int [] {x, y});
-        int selectedNum = map[x][y];
+        visited[pos.x][pos.y] = true;
+        int blockNumber = map[pos.x][pos.y];
+        map[pos.x][pos.y] = -2;
+        queue.offer(pos);
 
         while(!queue.isEmpty()) {
-            int [] now = queue.poll();
-            map[now[0]][now[1]] = -2;
+            Pos now = queue.poll();
 
             for (int d = 0 ; d < 4 ; d++) {
-                int nx = now[0] + D[d][0];
-                int ny = now[1] + D[d][1];
+                int nx = now.x + D[d][0];
+                int ny = now.y + D[d][1];
 
-                if (isOverflow(nx, ny) || (map[nx][ny] != 0 && map[nx][ny] != selectedNum) || visited[nx][ny]) continue;
+                if (checkOverflow(nx, ny) || visited[nx][ny] || (map[nx][ny] != 0 && map[nx][ny] != blockNumber)) continue;
 
                 visited[nx][ny] = true;
-                queue.offer(new int [] {nx, ny});
+                map[nx][ny] = -2;
+                queue.offer(new Pos(nx, ny));
             }
         }
     }
-    static boolean isOverflow(int nx, int ny) {
+
+    static int[][] rotateMap (){
+        int [][] newMap = new int[N][N];
+
+        for (int j = 0 ; j < N ; j++) {
+            for (int i = 0 ; i < N ; i++) {
+                newMap[N-1-j][i] = map[i][j];
+            }
+        }
+
+        return newMap;
+    }
+
+    static boolean checkOverflow(int nx, int ny) {
         if (nx < 0 || ny < 0 || nx >= N || ny >= N) return true;
         return false;
     }
 
-    static void printMap() {
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < N ; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
-
