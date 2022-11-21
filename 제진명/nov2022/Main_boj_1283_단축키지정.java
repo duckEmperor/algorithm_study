eab9ba28f8926bd36301351dabc301dcb7233f16
JINMYEONG <1995jjm@naver.com>
2022-11-21 22:47:34 +0900

    ✅ success: boj 1283 단축키지정

--- 제진명/nov2022/Main_boj_1283_단축키지정.java
+++ 제진명/nov2022/Main_boj_1283_단축키지정.java
@@ -6,62 +6,58 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_boj_1283_단축키지정 {
+    static StringTokenizer st;
+    static boolean [] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        boolean [] visited = new boolean[26];
+        visited = new boolean[26];
 
         for (int i = 0 ; i < N ; i++) {
             String input = br.readLine();
-            st = new StringTokenizer(input);
 
-            // 먼저 단어 첫번째 글자가 단축키로 지정되어 있는지 확인.
-            String answer = "";
-            boolean isRegist = false;
-            boolean isFirst = true;
-            while(st.hasMoreTokens()) {
-                String word = st.nextToken();
-                String _word = word.toLowerCase();
-                if (!isFirst) answer += " ";
-                if (!isRegist && !visited[_word.charAt(0) - 'a']) {
-                    answer += "[" + word.charAt(0) + "]" + word.substring(1);
-                    visited[_word.charAt(0) - 'a'] = true;
-                    isRegist = true;
-                } else {
-                    answer += word;
-                }
-                isFirst = false;
-            }
+            int index = keyWordIndex(input);
 
-            if (isRegist) {
-                sb.append(answer + "\n");
-                continue;
-            };
-
-            String _input = input.toLowerCase();
-            int index = -1;
-            for (int j = 0 ; j < _input.length() ; j++) {
-                // 공백인경우
-                if (_input.charAt(j) - 'a' < 0 ) continue;
-                if (!visited[_input.charAt(j) - 'a']) {
-                    index = j;
-                    visited[_input.charAt(j) - 'a'] = true;
-                    break;
-                }
-            }
             if (index == -1) {
                 sb.append(input + "\n");
                 continue;
             }
-            answer = "";
+
+            String answer = "";
             answer += input.substring(0, index) + "[" + input.charAt(index) + "]" + input.substring(index + 1);
             sb.append(answer + "\n");
 
         }
 
         System.out.println(sb);
+    }
+
+    static int keyWordIndex (String words) {
+        String lowerWord = words.toLowerCase();
+        st = new StringTokenizer(lowerWord);
+        int index = -1;
+        int now = 0;
+        while(st.hasMoreTokens()) {
+            String word = st.nextToken();
+            if (!visited[word.charAt(0) - 'a']) {
+                visited[word.charAt(0) - 'a'] = true;
+                return now;
+            };
+            if (index == -1) {
+                for (int i = 1 ; i < word.length() ; i++) {
+                    if (!visited[word.charAt(i) - 'a']) {
+                        index = now + i;
+                        break;
+                    };
+                }
+            }
+            now += word.length() + 1;
+        }
+
+        if (index != -1) visited[lowerWord.charAt(index) - 'a'] = true;
+
+        return index;
     }
 }
