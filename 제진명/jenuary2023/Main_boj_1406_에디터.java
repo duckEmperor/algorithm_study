82421ab5503d0c2e657d3fecba481ce4301df67b
JINMYEONG <1995jjm@naver.com>
2023-01-04 20:39:58 +0900

    ✅ success: boj 1406 에디터

--- 제진명/jenuary2023/Main_boj_1406_에디터.java
+++ 제진명/jenuary2023/Main_boj_1406_에디터.java
@@ -3,6 +3,8 @@ package 제진명.jenuary2023;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -14,11 +16,11 @@ public class Main_boj_1406_에디터 {
 
         String str = br.readLine();
 
-        Stack<Character> left = new Stack<>();
+        Deque<Character> left = new LinkedList<>();
         Stack<Character> right = new Stack<>();
 
         for (int i = 0 ; i < str.length() ; i++) {
-            left.push(str.charAt(i));
+            left.add(str.charAt(i));
         }
 
         int N = Integer.parseInt(br.readLine());
@@ -29,18 +31,18 @@ public class Main_boj_1406_에디터 {
             char com = st.nextToken().charAt(0);
 
             if (com == 'P') {
-                left.push(st.nextToken().charAt(0));
+                left.add(st.nextToken().charAt(0));
             } else if (com == 'L') {
-                if (left.size() > 0) right.push(left.pop());
+                if (left.size() > 0) right.push(left.pollLast());
             } else if (com == 'B') {
-                if (left.size() > 0) left.pop();
+                if (left.size() > 0) left.removeLast();
             } else if (com == 'D') {
-                if (right.size() > 0) left.push(right.pop());
+                if (right.size() > 0) left.add(right.pop());
             }
         }
 
         while (left.size() > 0) {
-            right.push(left.pop());
+            sb.append(left.poll());
         }
 
         while (right.size() > 0) {
